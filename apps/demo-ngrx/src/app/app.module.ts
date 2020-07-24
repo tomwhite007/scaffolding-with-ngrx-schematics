e3894ca2e17e8322cd632b3b7e4437427a7d558f
Tom White <tomwhite007@gmail.com>
2020-07-24 08:04:20 +0100

    scaffold effects root

--- apps/demo-ngrx/src/app/app.module.ts
+++ apps/demo-ngrx/src/app/app.module.ts
@@ -5,15 +5,25 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, DummyModule, StoreModule.forRoot({}, { 
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true,
+  imports: [
+    BrowserModule,
+    DummyModule,
+    StoreModule.forRoot(
+      {},
+      {
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true,
+        },
       }
-    }), !environment.production ? StoreDevtoolsModule.instrument() : []],
+    ),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
