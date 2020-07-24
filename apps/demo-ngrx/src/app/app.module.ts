78712abd3b74789a92c28e9175469ce9da1a5d5a
Tom White <tomwhite007@gmail.com>
2020-07-24 07:49:04 +0100

    generate and import dummy module

--- apps/demo-ngrx/src/app/app.module.ts
+++ apps/demo-ngrx/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DummyModule } from './dummy/dummy.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, DummyModule],
   providers: [],
   bootstrap: [AppComponent],
 })
